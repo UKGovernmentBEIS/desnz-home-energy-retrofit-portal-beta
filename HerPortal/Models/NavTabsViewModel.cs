c9cbb1360dd7d6dfebbd9915df1c33c0ca95939b
Samuel Young <samuel.young@softwire.com>
2024-01-18 11:22:11 +0000

    PC-527: extract tabs out to own component

---
+++ HerPortal/Models/NavTabsViewModel.cs
@@ -0,0 +1,32 @@
+﻿using HerPortal.BusinessLogic.Models.Enums;
+
+namespace HerPortal.Models;
+
+public class NavTabsViewModel
+{
+    public enum Tab
+    {
+        LocalAuthorities,
+        Consortiums,
+        ReferralFiles,
+        SupportingDocuments
+    }
+
+    public Tab SelectedTab { get; }
+    
+    public UserRole Role { get; }
+
+    public string GetClass(Tab compareTab) => SelectedTab == compareTab
+        ? "app-primary-navigation__item app-primary-navigation__item--current"
+        : "app-primary-navigation__item";
+
+    public string GetAriaCurrent(Tab compareTab) => SelectedTab == compareTab
+        ? "page"
+        : "false";
+
+    public NavTabsViewModel(Tab selectedTab, UserRole viewRole)
+    {
+        SelectedTab = selectedTab;
+        Role = viewRole;
+    }
+}
