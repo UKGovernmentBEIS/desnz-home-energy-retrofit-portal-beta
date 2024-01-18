250058307020bcfbab7d6fbea993a1e1815e5b98
Samuel Young <samuel.young@softwire.com>
2024-01-18 12:09:59 +0000

    PC-527: PC-542: generate alphabet letters

---
+++ HerPortal/Models/Components/AlphabetFilterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HerPortal.BusinessLogic.Models.Enums;
+
+namespace HerPortal.Models;
+
+public class AlphabetFilterViewModel
+{
+    public enum Tab
+    {
+        LocalAuthorities,
+        Consortiums,
+        ReferralFiles,
+        SupportingDocuments
+    }
+    
+    public IEnumerable<IEnumerable<char>> AlphabetHalves { get; }
+
+    public AlphabetFilterViewModel()
+    {
+        var alphabetFirstHalf = Enumerable.Range(0, 13).Select(i => (char)('A' + i));
+        var alphabetSecondHalf = Enumerable.Range(0, 13).Select(i => (char)('N' + i));
+
+        AlphabetHalves = new[] { alphabetFirstHalf, alphabetSecondHalf };
+    }
+}
